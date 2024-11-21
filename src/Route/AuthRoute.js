45837febd818d2c7c8533e07febe9d7b3d9cacbd
sumitpathak914 <sumitpathakofficial914@gmail.com>
2024-11-21 17:58:29 +0530

    chanegs

--- src/Route/AuthRoute.js
+++ src/Route/AuthRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { registerUser, loginUser } = require('../Controller/Auth/Auth_controller');
 const { upload } = require('../multerConfig/multer_Config'); 
-const { getCurrentWifiSSID } = require('../Controller/Auth/Wifi_Controller');
+
 
 
 const Auth_Router = express.Router();
@@ -12,6 +12,6 @@ Auth_Router.post('/register',  registerUser);
 
 // Login API
 Auth_Router.post('/login', loginUser);
-Auth_Router.get('/current-ssid', getCurrentWifiSSID);
+
 
 module.exports = Auth_Router;
