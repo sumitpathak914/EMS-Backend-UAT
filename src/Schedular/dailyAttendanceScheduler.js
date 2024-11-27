2942c2d54f7f6c2a3db4d03ff0ab3f19a904df66
sumitpathak914 <sumitpathakofficial914@gmail.com>
2024-11-27 14:28:32 +0530

    chanegs

--- src/Schedular/dailyAttendanceScheduler.js
+++ src/Schedular/dailyAttendanceScheduler.js
@@ -1,10 +1,6 @@
-const cron = require('node-cron');
-const { initializeDailyRecords } = require('../Controller/Attendance/Attendance_Controller');
-
-// Schedule a task to run every day at 12:50 PM
-cron.schedule('55 12 * * *', async () => {
+cron.schedule('35 14 * * *', async () => {
     const currentDate = new Date().toISOString().split('T')[0]; // Get today's date in YYYY-MM-DD format
-    console.log(`Running daily attendance initialization at 12:50 PM for date: ${currentDate}`);
+    console.log(`Running daily attendance initialization at 2:35 PM for date: ${currentDate}`);
 
     try {
         await initializeDailyRecords(currentDate);
